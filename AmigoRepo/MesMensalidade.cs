e82c8ce37099718cffc1276d6655b647a0aabc65
Fernando Bastos <fe@pobox.com>
2016-03-09 11:39:32 -0300

    Inclusao de classes para Mensalidades e MesMensalidade

---
+++ AmigoRepo/MesMensalidade.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AmigoRepo
+{
+    public class MesMensalidade : EntidadeRepo, IMesMensalidade
+    {
+        int _ano;
+        public int Ano
+        {
+            get
+            {
+                return _ano;
+            }
+            set
+            {
+                if (_ano != value)
+                {
+                    _ano = value;
+                    OnPropertyChanged(nameof(Ano));
+                }
+            }
+        }
+        int _mes;
+        public int Mes
+        {
+            get
+            {
+                return _mes;
+            }
+            set
+            {
+                if (_mes != value)
+                {
+                    _mes = value;
+                    OnPropertyChanged(nameof(Mes));
+                }
+            }
+        }
+
+        IChaveValor<int, string> _situacao;
+        public IChaveValor<int, string> Situacao
+        {
+            get
+            {
+                return _situacao;
+            }
+            set
+            {
+                if (_situacao != value)
+                {
+                    _situacao = value;
+                    OnPropertyChanged(nameof(Situacao));
+                }
+            }
+        }
+        
+    }
+}
