cb25b8adc0af8d0bffc992bf9f321e874a2b3572
Teresa Thompson <teresaanne0101@gmail.com>
2016-03-09 14:02:06 -0300

    Classe telefone - precisa consertar ultima implementação Itelefone.numero

---
+++ AmigoRepo/Telefone.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AmigoRepo
+{
+    public class Telefone : EntidadeRepo, ITelefone
+    {
+        string _Ddd;
+        public string Ddd
+        {
+            get
+            {
+                return _Ddd;
+            }
+
+            set
+            {
+                if (value.Equals(_Ddd)) return;
+                _Ddd = value;
+                OnPropertyChanged(nameof(Ddd));
+            }
+        }
+        string _Descricao;
+        public string Descricao
+        {
+            get
+            {
+                return _Descricao;
+            }
+
+            set
+            {
+                if (value.Equals(_Descricao)) return;
+                _Descricao = value;
+                OnPropertyChanged(nameof(Descricao));
+            }
+        }
+
+        
+        string ITelefone.Numero
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
