99fb194e36f72dc09c37e2379a74b2a4f281efe3
Fernando Bastos <fe@pobox.com>
2016-03-21 18:16:17 -0300

    emprestimo bens

---
+++ AmigoRepo/Agendamento.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmigoRepo
+{
+    public class Agendamento:EntidadeRepo,IObservacao
+    {
+        string _nomePessoa;
+        public string NomePessoa
+        {
+            get
+            {
+                return _nomePessoa;
+            }
+            set
+            {
+                if (_nomePessoa != value)
+                {
+                    _nomePessoa = value;
+                    OnPropertyChanged(nameof(NomePessoa));
+                }
+            }
+        }
+        string _nomeAnimal;
+        public string NomeAnimal
+        {
+            get
+            {
+                return _nomeAnimal;
+            }
+            set
+            {
+                if (_nomeAnimal != value)
+                {
+                    _nomeAnimal = value;
+                    OnPropertyChanged(nameof(NomeAnimal));
+                }
+            }
+        }
+
+        DateTime? _dataAgenda;
+        public DateTime? DataAgenda
+        {
+            get
+            {
+                return _dataAgenda;
+            }
+            set
+            {
+                if (_dataAgenda != value)
+                {
+                    _dataAgenda = value;
+                    OnPropertyChanged(nameof(DataAgenda));
+                }
+            }
+        }
+        int _codigoExecucao;
+        public int CodigoExecucao
+        {
+            get
+            {
+                return _codigoExecucao;
+            }
+            set
+            {
+                if (_codigoExecucao != value)
+                {
+                    _codigoExecucao = value;
+                    OnPropertyChanged(nameof(CodigoExecucao));
+                }
+            }
+        }
+        int idVetOuClinica;
+        public int IdVetOuClinica
+        {
+            get
+            {
+                return idVetOuClinica;
+            }
+            set
+            {
+                if (idVetOuClinica != value)
+                {
+                    idVetOuClinica = value;
+                    OnPropertyChanged(nameof(IdVetOuClinica));
+                }
+            }
+        }
+
+        int _codigoStatus;
+        public int CodigoStatus
+        {
+            get
+            {
+                return _codigoStatus;
+            }
+            set
+            {
+                if (_codigoStatus != value)
+                {
+                    _codigoStatus = value;
+                    OnPropertyChanged(nameof(CodigoStatus));
+                }
+            }
+        }
+    }
+}
---
+++ AmigoRepo/BemMovel.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmigoRepo
+{
+    public class BemMovel:EntidadeRepo, IObservacao
+    {
+        DateTime _dataCadastro;
+        public DateTime DataCadastro
+        {
+            get
+            {
+                return _dataCadastro;
+            }
+            set
+            {
+                if (_dataCadastro != value)
+                {
+                    _dataCadastro = value;
+                    OnPropertyChanged(nameof(DataCadastro));
+                }
+            }
+        }
+
+
+        string _descricao;
+        public string Descricao
+        {
+            get
+            {
+                return _descricao;
+            }
+            set
+            {
+                if (_descricao != value)
+                {
+                    _descricao = value;
+                    OnPropertyChanged(nameof(Descricao));
+                }
+            }
+        }
+
+        int _tipoBem;
+        public int TipoBem
+        {
+            get
+            {
+                return _tipoBem;
+            }
+            set
+            {
+                if (_tipoBem != value)
+                {
+                    _tipoBem = value;
+                    OnPropertyChanged(nameof(TipoBem));
+                }
+            }
+        }
+        int _origem;
+        public int Origem
+        {
+            get
+            {
+                return _origem;
+            }
+            set
+            {
+                if (_origem != value)
+                {
+                    _origem = value;
+                    OnPropertyChanged(nameof(Origem));
+                }
+            }
+        }
+        string _nomeProprietario;
+        public string NomeProprietario
+        {
+            get
+            {
+                return _nomeProprietario;
+            }
+            set
+            {
+                if (_nomeProprietario != value)
+                {
+                    _nomeProprietario = value;
+                    OnPropertyChanged(nameof(NomeProprietario));
+                }
+            }
+        }
+
+
+        string _nomeResponsavelEmprestimo;
+        public string NomeResponsavelEmprestimo
+        {
+            get
+            {
+                return _nomeResponsavelEmprestimo;
+            }
+            set
+            {
+                if (_nomeResponsavelEmprestimo != value)
+                {
+                    _nomeResponsavelEmprestimo = value;
+                    OnPropertyChanged(nameof(NomeResponsavelEmprestimo));
+                }
+            }
+        }
+        string _nomeNomePegadorEmprestimo;
+        public string NomeNomePegadorEmprestimo
+        {
+            get
+            {
+                return _nomeNomePegadorEmprestimo;
+            }
+            set
+            {
+                if (_nomeNomePegadorEmprestimo != value)
+                {
+                    _nomeNomePegadorEmprestimo = value;
+                    OnPropertyChanged(nameof(NomeNomePegadorEmprestimo));
+                }
+            }
+        }
+        DateTime? _dataSaida;
+        public DateTime? DataSaida
+        {
+            get
+            {
+                return _dataSaida;
+            }
+            set
+            {
+                if (_dataSaida != value)
+                {
+                    _dataSaida = value;
+                    OnPropertyChanged(nameof(DataSaida));
+                }
+            }
+        }
+        DateTime? _dataRetorno;
+        public DateTime? DataRetorno
+        {
+            get
+            {
+                return _dataRetorno;
+            }
+            set
+            {
+                if (_dataRetorno != value)
+                {
+                    _dataRetorno = value;
+                    OnPropertyChanged(nameof(DataRetorno));
+                }
+            }
+        }
+        int _status;
+        public int Status
+        {
+            get
+            {
+                return _status;
+            }
+            set
+            {
+                if (_status != value)
+                {
+                    _status = value;
+                    OnPropertyChanged(nameof(Status));
+                }
+            }
+        }
+
+        int _localizacao;
+        public int Localizacao
+        {
+            get
+            {
+                return _localizacao;
+            }
+            set
+            {
+                if (_localizacao == value)
+                {
+                    return;
+                }
+                _localizacao = value;
+                OnPropertyChanged(nameof(Localizacao));
+            }
+        }
+    }
+}
